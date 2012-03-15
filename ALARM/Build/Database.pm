d41f73b68d5a21163cd1a113df9f3350c03ab926
Kevin Mihelich <leming@gmail.com>
2012-03-15 10:44:39 -0600

    database and repository version information for status command

--- ALARM/Build/Database.pm
+++ ALARM/Build/Database.pm
@@ -303,10 +303,16 @@ sub status{
     my ($self, $package) = @_;
     if(defined($package) && $package ne '') {
         foreach my $arch ('armv5', 'armv7') {
-            my $ar = $self->{dbh}->selectall_arrayref("select package, repo, done, fail, builder, git, abs, skip, del from abs inner join $arch as a on (abs.id = a.id) where package = ?", undef, $package);
+            my $ar = $self->{dbh}->selectall_arrayref("select package, repo, pkgver, pkgrel, done, fail, builder, git, abs, skip, del from abs inner join $arch as a on (abs.id = a.id) where package = ?", undef, $package);
             if( scalar(@{$ar}) ){ # 1 or more
                 foreach my $r (@{$ar}){
-                    my ($name, $repo, $done, $fail, $builder, $git, $abs, $skip, $del) = @{$r};
+                    my ($name, $repo, $pkgver, $pkgrel, $done, $fail, $builder, $git, $abs, $skip, $del) = @{$r};
+                    
+                    my ($repover, $reporel) = $self->{dbh}->selectrow_array("select pkgver, pkgrel from files where del = 0 and arch = ? and pkgbase = ? limit 1", undef, $arch, $package);
+                    if (!$repover) {
+                        $repover = 0;
+                        $reporel = 0;
+                    }
                     my $state = (!$done && !$fail?'unbuilt':(!$done&&$fail?'failed':($done && !$fail?'done':'???')));
                     if($builder && $state eq 'unbuilt'){
                         $state = 'building';
@@ -314,7 +320,7 @@ sub status{
                     $state = "skipped" if ($skip);
                     $state = "removed" if ($del);
                     my $source = ($git&&!$abs?'git':(!$git&&$abs?'abs':'indeterminate'));
-                    my $status= sprintf("[$arch] %s: repo=>%s, src=>%s, state=>%s",$name,$repo,$source,$state);
+                    my $status= sprintf("[$arch] %s ($pkgver-$pkgrel|$repover-$reporel): repo=>%s, src=>%s, state=>%s",$name,$repo,$source,$state);
                     $status .= sprintf(", builder=>%s",$builder) if $state eq 'building';
                     my $blocklist = $self->{dbh}->selectall_arrayref("select abs.repo, abs.package, arm.fail, abs.skip, abs.del from package_name_provides as pn
                                                                      inner join package_depends as pd on (pn.package = pd.package)
