9b8c48b5791d843fe2da7535e2e9c1e2dce79029
Kevin Mihelich <kevin@archlinuxarm.org>
2012-12-08 05:46:49 -0500

    rrd file creation and update

--- ALARM/Build/Stats.pm
+++ ALARM/Build/Stats.pm
@@ -70,7 +70,8 @@ sub cb_queue {
     }
 }
 
-sub log_open {
+# farm host 7-day averaging log
+sub log_open_host {
     my ($self, $cn) = @_;
     
     my $self->{$cn} = RRDTool::OO->new(file => "$Bin/rrd/$cn.rrd");
@@ -81,17 +82,42 @@ sub log_open {
     # otherwise, create the file
     $self->{arch}->create(
         step        => 10,  # 10 second intervals
-        data_source => { name      => "mydatasource",
-                         type      => "GAUGE" },
-        archive     => { rows      => 5 });
+        data_source => { name   => "cpu0_user",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu0_system",   type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu0_wait",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu1_user",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu1_system",   type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu1_wait",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu2_user",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu2_system",   type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu2_wait",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu3_user",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu3_system",   type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "cpu3_wait",     type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "memory",        type    => "GAUGE",     min     => 0 },
+        data_source => { name   => "eth_r",         type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "eth_w",         type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "sd_ops_r",      type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "sd_ops_w",      type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "sd_oct_r",      type    => "DERIVE",    min     => 0 },
+        data_source => { name   => "sd_oct_w",      type    => "DERIVE",    min     => 0 },
+        archive     => { rows   => 1000,         cpoints    => 60,        cfunc     => "AVERAGE" });
     
 }
 
 sub log_stat {
     my ($self, $cn, $ts, $type, $value, $pkg) = @_;
     
-    $self->log_open if (!defined $self->{$cn});
+    # open host averaging log
+    $self->log_open_host($cn) if (!defined $self->{$cn});
     
+    # add data point
+    $self->{$cn}->update(time => $ts, values => { $type => $value });
+    
+    # store package data
+    if ($pkg ne '') {
+        # update stats table
+    }
 }
 
 1;
