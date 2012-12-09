a333a398a7a35ac1bdb7e00ea9e07e2c40294221
Kevin Mihelich <kevin@archlinuxarm.org>
2012-12-09 00:51:54 -0700

    graph generation

--- ALARM/Build/Stats.pm
+++ ALARM/Build/Stats.pm
@@ -14,6 +14,9 @@ use Text::CSV;
 use DBI;
 use RRDTool::OO;
 
+# set timezone for graphs
+$ENV{TZ}="MST";
+
 our $available = Thread::Semaphore->new(1);
 
 our ($q_svc, $q_db, $q_irc, $q_mir, $q_stats);
@@ -72,13 +75,13 @@ sub cb_queue {
 sub log_open_host {
     my ($self, $cn) = @_;
     
-    $self->{$cn} = RRDTool::OO->new(file => "$Bin/rrd/$cn.rrd", raise_error => 0);
+    $self->{host}->{$cn} = RRDTool::OO->new(file => "$Bin/rrd/$cn.rrd", raise_error => 0);
     
     # RRD file already exists
     return if (-f "$Bin/rrd/$cn.rrd");
     
     # otherwise, create the file
-    $self->{$cn}->create(
+    $self->{host}->{$cn}->create(
         step        => 10,  # 10 second intervals
         data_source => { name   => "cpu0_user",     type    => "GAUGE",     min     => 0 },
         data_source => { name   => "cpu0_system",   type    => "GAUGE",     min     => 0 },
@@ -110,13 +113,61 @@ sub log_stat {
     $self->log_open_host($cn) if (!defined $self->{$cn});
     
     # add data point
-    $self->{$cn}->update(time => $ts, values => $data);
+    $self->{host}->{$cn}->update(time => $ts, values => $data);
     
     # store package data
     #if ($pkg ne '') {
     #    $self->{dbh}->do("insert into stats (package, host, ts, $type) values ((select id from abs where package = ?), (select id from stat_hosts where name = ?), ?, ?)
     #                      on duplicate key update $type = ?", undef, $pkg, $cn, $ts, $value, $value);
     #}
+}
+
+sub log_graph_host {
+    foreach my $host (keys %{$self->{host}}) {
+        # build cdef for combined CPU usage
+        my ($cpus) = $self->{dbh}->selectrow_array("select cpus from stat_hosts where name = ?", undef, $host);
+        my $cdef_user = "cpu0_user,";
+        my $cdef_system = "cpu0_system,";
+        my $cdef_wait = "cpu0_wait,";
+        for (my $i = 1; $i < $cpus; $i++) {
+            $cdef_user .= "cpu" . $i . "_user,+";
+            $cdef_system .= "cpu" . $i . "_system,+";
+            $cdef_wait .= "cpu" . $i . "_wait,+";
+        }
+        $cdef_user .= "$cpus,/";
+        $cdef_system .= "$cpus,/";
+        $cdef_wait .= "$cpus,/";
+        
+        # create graph
+        $self->{host}->{$host}->graph(
+            image          => "$self->{packaging}->{imageroot}/$host.png",
+            start          => '-1w',
+            width          => 1000,
+            height         => 200,
+            y_grid         => '25:4',
+            x_grid         => 'HOUR:12:DAY:1:DAY:1:86400:%A',
+            slope_mode     => undef,
+            title          => "$host Average CPU and Memory Usage",
+            vertical_label => 'Percentage',
+            draw           => { type    => 'hidden',    dsname  => 'cpu0_user',     name    => 'cpu0_user',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu1_user',     name    => 'cpu1_user',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu2_user',     name    => 'cpu2_user',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu3_user',     name    => 'cpu3_user',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu0_system',   name    => 'cpu0_system',   cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu1_system',   name    => 'cpu1_system',   cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu2_system',   name    => 'cpu2_system',   cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu3_system',   name    => 'cpu3_system',   cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu0_wait',     name    => 'cpu0_wait',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu1_wait',     name    => 'cpu1_wait',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu2_wait',     name    => 'cpu2_wait',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'cpu3_wait',     name    => 'cpu3_wait',     cfunc     => 'AVERAGE' },
+            draw           => { type    => 'hidden',    dsname  => 'mem',           name    => 'mem',           cfunc     => 'AVERAGE' },
+            draw           => { type    => 'area',      color   => '00FF00',        legend  => 'CPU User',      cdef      => $cdef_user },
+            draw           => { type    => 'area',      color   => '0000FF',        legend  => 'CPU System',    cdef      => $cdef_system,  stack  => 1 },
+            draw           => { type    => 'area',      color   => 'FF0000',        legend  => 'CPU Wait',      cdef      => $cdef_wait,    stack  => 1 },
+            draw           => { type    => 'line',      color   => '000000',        legend  => 'Memory',        cdef      => 'mem,10,/' },
+        );
+    }
 }
 
 1;
